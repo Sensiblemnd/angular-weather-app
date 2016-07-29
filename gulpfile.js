3e1f7ea39af47bdc1f05da7ad2d0b52920070ef5
Rob Lloveras <sensiblemnd@gmil.com>
2016-07-29 15:03:48 -0400

    Setting up sourcemaps csso

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,10 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var bs = require('browser-sync').create();
 var sourcemaps = require('gulp-sourcemaps');
+var prefix = require('gulp-autoprefixer');
+var csso = require('gulp-csso');
+var gulpif = require('gulp-if');
+
 
 gulp.task('browser-sync', ['sass'], function() {
     bs.init({
@@ -13,9 +17,14 @@ gulp.task('browser-sync', ['sass'], function() {
 
 gulp.task('sass', function () {
     return gulp.src('scss/*.scss')
-                .pipe(sass())
+                .pipe(sourcemaps.init())
+                .pipe(sass().on('error', sass.logError))
+                .pipe(prefix('last 1 version'))
+                .pipe(csso())
+                .pipe(sourcemaps.write())
                 .pipe(gulp.dest('css'))
                 .pipe(bs.reload({stream: true}));
+
 });
 
 gulp.task('watch', ['browser-sync'], function () {
